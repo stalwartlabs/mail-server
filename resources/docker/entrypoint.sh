2938b98e1827cd9508962e61b40718b62281bc3d
mdecimus <mauro@stalw.art>
2023-10-26 10:01:14 +0200

    Fixed Docker entrypoint

--- resources/docker/entrypoint.sh
+++ resources/docker/entrypoint.sh
@@ -2,7 +2,7 @@
 # shellcheck shell=dash
 
 # If the configuration file does not exist wait until it does.
-while [ ! -f /opt/stalwart-mail/etc/config.toml ]; do
+while [ ! -f /opt/stalwart-mail/etc/config.toml ] || grep -q "__CERT_PATH__" /opt/stalwart-mail/etc/common/tls.toml; do
     sleep 1
 done
 
